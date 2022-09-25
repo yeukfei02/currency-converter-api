e902d91af849cc9a0f3349e42e54d347014b27f9
Donald Wu <yeukfei02@gmail.com>
2022-09-25 23:50:44 +0800

    fix: 🐛 update main api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getMain() {
+    const data = {
+      message: 'currency-converter-api',
+    };
+    return data;
   }
 }
