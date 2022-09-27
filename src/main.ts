478f3dbac59f4333a1761830d9f2403a7b85b330
Donald Wu <yeukfei02@gmail.com>
2022-09-27 20:29:12 +0800

    feat: 🎸 enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+
+  // middleware
+  app.enableCors();
+
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
